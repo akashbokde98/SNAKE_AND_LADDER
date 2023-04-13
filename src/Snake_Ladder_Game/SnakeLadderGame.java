5a0842df4097e72657fd532bb9482755d9c0cc82
akashbokde98 <akashbokde98@gmail.com>
2023-04-13 17:37:14 +0530

    Added:Welcome Message

---
+++ src/Snake_Ladder_Game/SnakeLadderGame.java
@@ -0,0 +1,9 @@
+package Snake_Ladder_Game;
+
+public class SnakeLadderGame
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Snake And Ladder Problem!");
+    }
+}
