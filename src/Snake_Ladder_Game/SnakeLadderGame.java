e579506ec28a79f9bf01688ca25913112681b3ad
akashbokde98 <akashbokde98@gmail.com>
2023-04-13 19:50:42 +0530

    Added:Checked For Playing Option

--- src/Snake_Ladder_Game/SnakeLadderGame.java
+++ src/Snake_Ladder_Game/SnakeLadderGame.java
@@ -1,13 +1,29 @@
 package Snake_Ladder_Game;
 import java.lang.Math;
+import java.util.Random;
 
 public class SnakeLadderGame
 {
     public static void main(String[] args)
     {
+        Random random = new Random();
         System.out.println("Welcome To Snake And Ladder Problem!");
         int playerPosition = 0;
         int dieRolls = (int)((Math.random() * 6) + 1); //Generating Random Numbers 0-6.
+        //Checking Option Whether Player Want to No Play,Ladder or Snake.
+        switch (random.nextInt(3))
+        {
+            case 1: //Case of Ladder.
+                playerPosition += dieRolls;
+                break;
+
+            case 2: //Case of Snake.
+                playerPosition -= dieRolls;
+                break;
+
+            default: //Case of No Play.
+                System.out.println("No Play!");
+        }
     }
 }
 
