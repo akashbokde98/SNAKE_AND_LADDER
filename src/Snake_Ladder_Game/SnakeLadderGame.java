4fdd99465a3e3c3502ae9b0a739bcce60b09d0ee
akashbokde98 <akashbokde98@gmail.com>
2023-04-14 18:13:36 +0530

    Added:Finalized The Winnig Player

--- src/Snake_Ladder_Game/SnakeLadderGame.java
+++ src/Snake_Ladder_Game/SnakeLadderGame.java
@@ -8,16 +8,21 @@ public class SnakeLadderGame
     {
         Random random = new Random();
         System.out.println("Welcome To Snake And Ladder Problem!");
-        int playerPosition = 0;
+        int firstPlayerPosition = 0;
+        int secondPlayerPosition = 0;
         int winningPosition = 100;
         int numberOfDice = 0;
-
-        System.out.println("Player At Position:" + playerPosition);
+        //Player's Initial Position.
+        System.out.println("Player1 At Position:" + firstPlayerPosition);
+        System.out.println("Player2 At Position:" + secondPlayerPosition);
         // Loop Will Run till the Player Reaches the Winning Position 100.
-        while (playerPosition < winningPosition )
+        while (firstPlayerPosition < winningPosition && secondPlayerPosition < winningPosition)
         {
-            if (playerPosition >= 0)
+
+            // Player 1
+            if (firstPlayerPosition >= 0)
             {
+                System.out.println("------Player1-------");
                 //Generating Random Numbers 1-6 By Rolling Dice.
                 int diceRolls = (int)((Math.random() * 6) + 1);
                 System.out.println("Dice Rolls:" + diceRolls);
@@ -25,40 +30,95 @@ public class SnakeLadderGame
                 switch (random.nextInt(3))
                 {
                     case 1: //Case of Ladder.
-                        int x = playerPosition + diceRolls;
+                        int p1 = firstPlayerPosition + diceRolls;
                         //Condition To Get Exact Position.
-                        if (x <= winningPosition)
+                        if (p1 <= winningPosition)
                         {
-                            playerPosition += diceRolls;
+                            firstPlayerPosition += diceRolls;
                             System.out.println("Ladder By:" + diceRolls);
                         }
-                        System.out.println("Player At Position:" + playerPosition);//Checking Player's Current Position in Game.
+                        System.out.println("Player1 At Position:" + firstPlayerPosition);//Checking Player's Current Position in Game.
                         System.out.println("--------------------");
                         numberOfDice++;
-                        break;
+                        continue;
 
                     case 2: //Case of Snake.
-                        playerPosition -= diceRolls;
+                        firstPlayerPosition -= diceRolls;
                         System.out.println("Snake By:" + diceRolls);
-                        System.out.println("Player At Position:" + playerPosition);//Checking Player's Current Position in Game.
+                        System.out.println("Player1 At Position:" + firstPlayerPosition);//Checking Player's Current Position in Game.
                         System.out.println("--------------------");
                         numberOfDice++;
                         break;
 
                     default: //Case of No Play.
                         System.out.println("No Play!");
-                        System.out.println("Player At Position:" + playerPosition);//Checking Player's Current Position in Game.
+                        System.out.println("Player1 At Position:" + firstPlayerPosition);//Checking Player's Current Position in Game.
                         System.out.println("--------------------");
                 }
+               // continue;
             }
             else
             {
                 //In Case Player Position Moves Below 0, then player restart from 0.
-                playerPosition = 0;
+                firstPlayerPosition = 0;
+                System.out.println("Restart From '0' Position");
+            }
+
+            // Player2
+            if (secondPlayerPosition >= 0)
+            {
+                System.out.println("------Player2-------");
+                //Generating Random Numbers 1-6 By Rolling Dice.
+                int diceRolls = (int)((Math.random() * 6) + 1);
+                System.out.println("Dice Rolls:" + diceRolls);
+                //Checking Option Whether Player Want to No Play,Ladder or Snake.
+                switch (random.nextInt(3))
+                {
+                    case 1: //Case of Ladder.
+                        int p2 = secondPlayerPosition + diceRolls;
+                        //Condition To Get Exact Position.
+                        if (p2 <= winningPosition)
+                        {
+                            secondPlayerPosition += diceRolls;
+                            System.out.println("Ladder By:" + diceRolls);
+                        }
+                        System.out.println("Player2 At Position:" + secondPlayerPosition);//Printing Player2 Current Position After Getting Ladder.
+                        System.out.println("--------------------");
+                        numberOfDice++;
+                        continue;
+
+                    case 2: //Case of Snake.
+                        secondPlayerPosition -= diceRolls;
+                        System.out.println("Snake By:" + diceRolls);
+                        System.out.println("Player2 At Position:" + secondPlayerPosition);//Printing Player2 Current Position After Getting Snake Bite.
+                        System.out.println("--------------------");
+                        numberOfDice++;
+                        break;
+
+                    default: //Case of No Play.
+                        System.out.println("No Play!");
+                        System.out.println("Player2 At Position:" + secondPlayerPosition);//Printing Player2 Current Position After Choosing No Play Option.
+                        System.out.println("--------------------");
+                }
+                //continue;
+            }
+            else
+            {
+                //In Case Player Position Moves Below 0, then player restart from 0.
+                secondPlayerPosition = 0;
                 System.out.println("Restart From '0' Position");
             }
         }
-        System.out.println("Player At Winning Position:" + playerPosition);
+        //Checking Which Player Come First To Winning Position.
+        if (firstPlayerPosition == winningPosition)
+        {
+            System.out.println("Player1 At Winning Position:" + firstPlayerPosition);
+        }
+        else
+        {
+            System.out.println("Player2 At Winning Position:" + secondPlayerPosition);
+        }
+
         System.out.println("Number of Times the Dice Was Played is:" + numberOfDice);
     }
 }
